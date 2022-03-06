0ca31272b2262b07528192779c45baf27e8896df
Ramses Di Perna <ramses@sparklingsociety.net>
2022-03-06 13:37:43 +0100

    Added NestedSO tests

---
+++ Assets/_Core/SOCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NestedSO;
+
+[CreateAssetMenu()]
+public class SOCollection : NestedSOCollectionBase<SOCollectionItem>
+{
+
+}
---
+++ Assets/_Core/SOCollectionItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SOCollectionItem : ScriptableObject
+{
+	[SerializeField]
+	private string _fieldA = null;
+
+	[SerializeField]
+	private string[] _fieldB = null;
+}
